9e63a6182ec9172f0606261ebfe7a9094c9225ba
Магомедали Гаджиев <mag198421@gmail.com>
2025-07-13 11:12:37 +0300

    Добавил классы Species и Breed

---
+++ PetFamily.Backend/src/PetFamily.Domain/Species/Breed.cs
@@ -0,0 +1,20 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace PetFamily.Domain.Species
+{
+    public class Breed : Entity<int>
+    {
+        private string Name { get; private set; }
+
+        public Breed(string name)
+        {
+            Name = name;
+        }
+
+        public static Result<Breed> Create(string name)
+        {
+            //Валидация
+            return Result.Success(new Breed(name));
+        }
+    }
+}
---
+++ PetFamily.Backend/src/PetFamily.Domain/Species/Species.cs
@@ -0,0 +1,24 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace PetFamily.Domain.Species
+{
+    public class Species : Entity<int>
+    {
+        public string Name { get; private set; }
+        public IReadOnlyCollection<Breed> Breeds => _breeds.AsReadOnly();
+
+
+        private readonly List<Breed> _breeds = [];
+
+        private Species(string name)
+        {
+            Name = name;
+        }
+
+        public Result<Species> Create(string name)
+        {
+            // Валидация
+            return Result.Success(new Species(name));
+        }
+    }
+}
