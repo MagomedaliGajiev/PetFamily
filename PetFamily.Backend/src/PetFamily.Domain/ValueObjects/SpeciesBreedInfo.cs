72406b0694436606b485ff81158d3319728027b9
Магомедали Гаджиев <mag198421@gmail.com>
2025-07-13 10:59:20 +0300

    Добавил SpeciesBreedInfo

---
+++ PetFamily.Backend/src/PetFamily.Domain/ValueObjects/SpeciesBreedInfo.cs
@@ -0,0 +1,33 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace PetFamily.Domain.ValueObjects
+{
+    public class SpeciesBreedInfo : ValueObject
+    {
+        public int SpeciesId { get; }
+        public int BreedId { get; }
+
+        private SpeciesBreedInfo(int speciesId, int breedId)
+        {
+            SpeciesId = speciesId;
+            BreedId = breedId;
+        }
+
+        public static Result<SpeciesBreedInfo> Create(int speciesId, int breedId)
+        {
+            if (speciesId <= 0)
+                return Result.Failure<SpeciesBreedInfo>("Invalid species ID");
+
+            if (breedId <= 0)
+                return Result.Failure<SpeciesBreedInfo>("Invalid breed ID");
+
+            return Result.Success(new SpeciesBreedInfo(speciesId, breedId));
+        }
+
+        protected override IEnumerable<IComparable> GetEqualityComponents()
+        {
+            yield return SpeciesId;
+            yield return BreedId;
+        }
+    }
+}
