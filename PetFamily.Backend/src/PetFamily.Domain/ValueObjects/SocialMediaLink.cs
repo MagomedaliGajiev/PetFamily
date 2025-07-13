aa2c04997324587bf33cd1c2bdf55c49b4aa84de
Магомедали Гаджиев <mag198421@gmail.com>
2025-07-13 10:43:39 +0300

    Добавил класс SocialMediaLink

---
+++ PetFamily.Backend/src/PetFamily.Domain/ValueObjects/SocialMediaLink.cs
@@ -0,0 +1,28 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace PetFamily.Domain.ValueObjects
+{
+    public class SocialMediaLink : ValueObject
+    {
+        public string Name { get; }
+        public string Url { get; }
+
+        private SocialMediaLink(string name, string url)
+        {
+            Name = name;
+            Url = url;
+        }
+
+        public static Result<SocialMediaLink> Create(string name, string url)
+        {
+            //придумать логику валидации
+            return Result.Success(new SocialMediaLink(name, url));
+        }
+
+        protected override IEnumerable<IComparable> GetEqualityComponents()
+        {
+            yield return Name;
+            yield return Url;
+        }
+    }
+}
